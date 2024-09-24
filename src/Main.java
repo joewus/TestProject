a72b861c91947a3ff958fd87a7a625a03ddfff7d
Joseph <joewus078@gmail.com>
2024-09-24 17:08:51 +0200

    Fixed the bugs and updated AccountService.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Account account = new Account(123, "Chang", 50 );
+        AccountService accountserivce = new AccountService(account);
+
+        System.out.println(accountserivce.getBalance());
+        accountserivce.deposit(accountserivce);
+        System.out.println(accountserivce.getBalance());
     }
 }
