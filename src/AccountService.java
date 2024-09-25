6ca53a7f5e58bcbb83bb415a4c4cb6459f333432
Joseph <joewus078@gmail.com>
2024-09-25 16:04:59 +0200

    Fixed the bugs and updated AccountService.java

--- src/AccountService.java
+++ src/AccountService.java
@@ -1,66 +1,52 @@
-import java.util.Scanner; // Import the Scanner class
+import java.util.Scanner;
 
 public class AccountService extends Account {
-
-    // Creating an instance of the Scanner class
     private Scanner scanner = new Scanner(System.in);
 
-    public AccountService(Account account) {
-        super(account.getId(), account.getName(), account.getBalance());
+    // Constructor for AccountService class
+    public AccountService() {
+        super(); // Calls the parent class (Account) constructor
     }
 
-    // Deposits the specified amount into the given account
-
-    public void deposit(Account account) {
+    // Deposits the specified amount into this account
+    public void deposit(Account account){
         System.out.print("Enter amount to deposit: ");
-
-        // Taking deposit amount from the user
         int amount = scanner.nextInt();
 
-        // Ensuring the user enters a valid deposit amount
         if (amount > 0) {
-            account.setBalance(account.getBalance() + amount);
+            setBalance(account.getBalance() + amount); // Update balance using setBalance
             System.out.println("Deposited: " + amount);
         } else {
             System.out.println("Deposit amount must be positive.");
         }
     }
 
-    // Withdraws the specified amount from the given account.
-
+    // Withdraws the specified amount from this account
     public void withdraw(Account account) {
         System.out.print("Enter amount to withdraw: ");
-
-        // Taking withdrawal amount from the user
         int amount = scanner.nextInt();
 
-        // Ensuring the user enters a valid withdrawal amount
-        if (amount > 0) {
-            account.withdraw(amount);
+        if (amount > 0 && amount <= getBalance()) {
+            setBalance(account.getBalance() - amount); // Update balance using setBalance
             System.out.println("Withdrew: " + amount);
         } else {
-            System.out.println("Invalid withdraw amount.");
+            System.out.println("Insufficient funds for withdrawal.");
         }
     }
 
+    // Transfers the specified amount from one account to another
     public void transfer(Account fromAccount, Account toAccount) {
         System.out.print("Enter amount to transfer: ");
-
-        // Taking transfer amount from the user
         int amount = scanner.nextInt();
 
-        if (amount > 0 && amount <= getBalance(fromAccount)) {
-            fromAccount.withdraw(amount);
-            toAccount.deposit(amount);
-
-            System.out.println("Transferred: " + amount + " to Account ID: " + getId(toAccount));
+        if (amount > 0 && amount <= fromAccount.getBalance()) {
+            fromAccount.setBalance(fromAccount.getBalance() - amount); // Deduct from current account
+            toAccount.setBalance(toAccount.getBalance() + amount); // Add to the recipient account
+            System.out.println("Transferred: " + amount + " to Account ID: " + toAccount.getId());
         } else {
             System.out.println("Invalid transfer amount.");
         }
-
     }
-
-    //  Closes the scanner to prevent resource leaks.
 
     public void closeScanner() {
         scanner.close();
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Account account = new Account(123, "Chang", 50);
-        AccountService accountserivce = new AccountService(account);
+        // Create two accounts
+        Account account1 = new Account(123, "Chang", 100);
+        Account account2 = new Account(456, "Luiz", 50);
 
-        System.out.println(accountserivce.getBalance());
-        accountserivce.deposit(accountserivce);
-        System.out.println(accountserivce.getBalance());
+        // Create AccountService for account1 (AccountService extends Account)
+        AccountService accountService1 = new AccountService();
+
+
+        // Transfer between account1 and account2 using the transfer method
+        accountService1.transfer(account1, account2); // Pass both accounts for the transfer
+
+        System.out.println(account1.getBalance());
+        System.out.println(account2.getBalance());
+        accountService1.withdraw(account1);
+        System.out.println(accountService1.getBalance(account2));
+        // Close the scanner
+        accountService1.closeScanner();
     }
 }
